00aaf08068fb60fd13b8661aae4ae15058d8927e
IsacHolstad <wisac22@gmail.com>
2022-06-13 00:58:42 +0200

    async await

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 const apiURL = "https://wger.de/api/v2/exercisecategory/?format=json";
 console.log(apiURL)
+const container = document.querySelector(".container");
+
+async function workoutPlace () {
+    try{
+        const response = await fetch(apiURL)
+        console.log(response)
+    }
+    catch(error){
+        container.innerHTML = `<h1>404</h1>`
+    }
+
+
+}
+workoutPlace();
