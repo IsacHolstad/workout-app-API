b95ce0ae2fef14eda5171330a0b8b87fc81c4e19
IsacHolstad <wisac22@gmail.com>
2022-06-13 01:02:40 +0200

    full async await

--- js/script.js
+++ js/script.js
@@ -6,6 +6,13 @@ async function workoutPlace () {
     try{
         const response = await fetch(apiURL)
         console.log(response)
+        const responseJSON = await response.json();
+        console.log(responseJSON);
+        const workoutData = responseJSON.data;
+        console.log(workoutData);
+        for (let i = 0; workoutData.length; i++){
+            console.log(workoutData[i].results)
+        }
     }
     catch(error){
         container.innerHTML = `<h1>404</h1>`
