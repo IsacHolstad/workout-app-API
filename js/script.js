4f480a6575713882bb9f789f4435204efb9762e8
IsacHolstad <wisac22@gmail.com>
2022-06-13 21:04:31 +0200

    debugg js code

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-const apiURL = "https://wger.de/api/v2/exercisecategory/?format=json";
+const apiURL = "https://wger.de/api/v2/exercisecategory/?format=json&=cdbdf424b250d470cd66c299d69a3d47c6eba32c";
 console.log(apiURL)
 const proxy = "https://noroffcors.herokuapp.com/";
 const corsFixUrl = proxy + apiURL;
 const container = document.querySelector(".container");
 
-async function workoutPlace () {
+const cryptoContainer = document.querySelector(".container");
+
+async function workoutNames() {
     try{
-        const response = await fetch(corsFixUrl)
+        const response = await fetch(corsFixUrl);
         console.log(response)
         const responseJSON = await response.json();
-        console.log(responseJSON);
-        const workoutData = responseJSON.data;
-        console.log(workoutData);
-        for (let i = 0; i < responseJSON.length; i++){
+        console.log(responseJSON)
+        const cryptoData = responseJSON.data;
+        console.log(cryptoData)
+        for (let i = 0; i < responseJSON.length; i++) {
             console.log(responseJSON[i].results.name)
-            container.innerHTML += `<li>${responseJSON[i].results.name}</li>`;
+            
+            container.innerHTML += `<li><a href="detailcrypto.html?id=${cryptoData[i].id}">${cryptoData[i].id} ${cryptoData[i].supply}</a></li>`;
         }
     }
-    catch(error){
-        container.innerHTML += `<h1>404</h1>`
+    catch(error) {
+        container.innerHTML += `<li>404</li>`
+       
     }
-
-
 }
-workoutPlace();
+workoutNames();
