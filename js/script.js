a128dbab7028c14ec21fdc7da26e720ede3e1f8a
IsacHolstad <wisac22@gmail.com>
2022-06-17 15:30:46 +0200

    no need for corsfix

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 const apiURL = "https://wger.de/api/v2/exercisecategory/?format=json&=cdbdf424b250d470cd66c299d69a3d47c6eba32c";
 console.log(apiURL)
-const proxy = "https://noroffcors.herokuapp.com/";
-const corsFixUrl = proxy + apiURL;
-const container = document.querySelector(".container");
 
+const container = document.querySelector(".container");
 const cryptoContainer = document.querySelector(".container");
 
 (async function workoutNames() {
@@ -16,13 +14,12 @@ const cryptoContainer = document.querySelector(".container");
         console.log(cryptoData)
         for (let i = 0; i < cryptoData.length; i++) {
             console.log(cryptoData[i].results);
-            
+        
             container.innerHTML += `<li class="workout-list">${cryptoData[i].name} </li>`;
         }
     }
     catch(error) {
         container.innerHTML = `<li class="error-message">error</li>`
-       
     }
 }
 )();
