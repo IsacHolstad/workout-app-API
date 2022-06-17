129ea9f2dd5c59bea35aaedd3dad0599ed12820b
IsacHolstad <wisac22@gmail.com>
2022-06-17 13:54:06 +0200

    data box style

--- js/script.js
+++ js/script.js
@@ -12,16 +12,16 @@ async function workoutNames() {
         console.log(response)
         const responseJSON = await response.json();
         console.log(responseJSON)
-        const cryptoData = responseJSON.data;
+        const cryptoData = responseJSON.results;
         console.log(cryptoData)
-        for (let i = 0; i < responseJSON.length; i++) {
-            console.log(responseJSON[i].results.name)
+        for (let i = 0; i < cryptoData.length; i++) {
+            console.log(cryptoData[i].results.id);
             
-            container.innerHTML += `<li>${responseJSON[i].results.name}</li>`;
+            container.innerHTML += `<li>${cryptoData[i].results.id}</li>`;
         }
     }
     catch(error) {
-        container.innerHTML += `<li>404</li>`
+        container.innerHTML = `<li class="error-message">error</li>`
        
     }
 }
